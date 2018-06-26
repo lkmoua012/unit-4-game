7f15ff523ec50a41a0a6608e8360b19ace22e7f3
larry.moua <lkmoua@gmail.com>
2018-06-26 16:49:05 -0700

    Hover and click function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,14 +26,14 @@ var Dooku {
 
 var fighter1 = what user picked.
 
-var fighter2 = what user picked for enemy.
+var fighter2 = what user picked for opponent.
 
 progress bars for hp?
 have progress bar 100% = total hp
 
 button that calls combat function
 
-COMBAT FUNCTION // Don't display until fighterChosen = true;
+COMBAT FUNCTION // Don't display until combatStart = true;
 Applies damage to both fighters.
 Display combat text.
 
@@ -43,7 +43,78 @@ Function that runs RNG and stores it into a variable
 Function for each animation play.
 Have combat randomize animation function.
 
+--- Game Flow ---
 
+1 - Choose fighter.
+    - Create an undo function.
+2 - Choose opponent.
+    - Create an undo function.
+3 - Click "fight" to initiate combat.
+4 - Click "attack" to reduce opponent's hp.
+5 - If opponent hp <= 0
+    - Player wins, update status text.
+    - Restore player hp.
+    - Choose new opponent.
+    - Trophy +1.
+  - If player hp <= 0
+    - Game Over.
+    - Click "New Game."
 
 
 */
+
+var anakin = {
+    hp: 100,
+    atk: 12,
+    c_atk: 15
+};
+
+var obi = {
+    hp: 150,
+    atk: 18,
+    c_atk: 20
+};
+
+var choseFighter1 = false;
+var choseFighter2 = false;
+
+$(function() {
+
+    $(".fightSprite").hide();
+
+    // Choose Fighter 1
+    $(".mugFighter").hover(function(){
+
+        console.log( 'hovering' );
+
+    // If hovering over Anakin and choseFighter1 = false
+       if ( $(this).is("#mugAnakin") && choseFighter1 == false ){
+            $("#anakinSprite1").toggle();
+    } else {
+        //    $("#anakinSprite2").toggle();
+    };
+
+    /*
+       if ($(this).is("#mugObi")){
+           console.log ("Choose Obi");
+    };
+
+       if ($(this).is("#mugWindu")){
+           console.log ("Choose Windu");
+    };
+
+       if ($(this).is("#mugDooku")){
+           console.log ("Choose Dooku");
+    };
+    */
+    //End Hover Function
+   });
+
+   $(".mugFighter").click(function(){
+
+    console.log("Fighter Chosen");
+
+   })
+
+// End function ready //   
+});
