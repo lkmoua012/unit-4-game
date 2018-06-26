9835f2a1a0499d3661a40ca6d565dcbb97243bab
Moua Larry <lkmoua@gmail.com>
2018-06-25 21:09:23 -0700

    Pseudo code and image assets

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+/*
+
+var Anakin {
+    hp: 125
+    atk: 12
+    c_atk: 15
+}
+
+var Obi-Wan {
+    hp: 150
+    atk: 18
+    c_atk: 20
+}
+
+var Windu {
+    hp: 175
+    atk: 21
+    c_atk: 30
+}
+
+var Dooku {
+    hp: 200
+    atk: 25
+    c_atk: 40
+}
+
+var fighter1 = what user picked.
+
+var fighter2 = what user picked for enemy.
+
+progress bars for hp?
+have progress bar 100% = total hp
+
+button that calls combat function
+
+COMBAT FUNCTION // Don't display until fighterChosen = true;
+Applies damage to both fighters.
+Display combat text.
+
+
+Function that runs RNG and stores it into a variable
+
+Function for each animation play.
+Have combat randomize animation function.
+
+
+
+
+*/
